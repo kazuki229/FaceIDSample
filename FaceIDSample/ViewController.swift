aebb46268621dd5866f435960cdab0f521e2783b
kazuki229 <tsuzuku.k@gmail.com>
2018-02-18 00:00:44 +0900

    Initial Commit

---
+++ FaceIDSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceIDSample
+//
+//  Created by 都筑一希 on 2018/02/18.
+//  Copyright © 2018年 kazuki229. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
